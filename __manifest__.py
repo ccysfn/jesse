9d1921a36880cf24b5582e396b9c3f5d248849ee
ccysfn <jessecao911@gmail.com>
2019-06-04 10:40:06 +0800

    aswe

--- __manifest__.py
+++ __manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'views/parents.xml',
         'views/views.xml',
         'views/templates.xml',
         'views/user.xml'
