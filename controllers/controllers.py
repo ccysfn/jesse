e7bf11538922a62c3ae7aee274425eb77f3f864f
ccysfn <jessecao911@gmail.com>
2019-06-13 15:10:07 +0800

    423

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -4,10 +4,11 @@ from odoo import http
 class Jyinspur(http.Controller):
     @http.route('/jyinspur/soft/', auth='public')
     def index(self, **kw):
-        Softs=http.request.env['jyinspur.soft']
-        return http.request.render('jyinspur.index',{
-            'soft':Softs.search([])
-        })
+        #Softs = http.request.env['jyinspur.soft']
+        #return http.request.render('jyinspur.index',{
+         #   'soft':Softs.search([])
+        #})
+        return "hello"
 
     @http.route('/jyinspur/jyinspur/objects/', auth='public')
     def list(self, **kw):
